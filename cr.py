0df887101537260c88fac78ad93aba7043a65f53
markus1258 <markus1258@gmail.com>
2022-03-29 19:59:19 +0200

    Added requirements.txt and Dockerfile for Docker

--- cr.py
+++ cr.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def blank():
+    return "Yo"
+
 @app.route("/_health")
 def health():
-    return 200
+    return "200"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
