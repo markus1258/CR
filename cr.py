1b4b3ccefe0289df8fc96053e0a4e83d3d3875de
cr-omricohen <54633400+cr-omricohen@users.noreply.github.com>
2022-03-30 13:07:00 +0300

    a non breaking commit (#7)
    * Some commits
    Co-authored-by: omri.cohen <omri.cohen@cybereason.com>

--- cr.py
+++ cr.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def blank():
     return "Yody!!"
 
+
 @app.route("/_health")
 def health():
     return "200"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
